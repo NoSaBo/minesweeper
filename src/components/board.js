afe0033d24c8c6dd95ca31ba471829a437a47594
Jesus Rey <jesusr@oktana.io>
2019-11-08 18:34:04 -0500

    update logic and structure for createBoard

--- src/components/board.js
+++ src/components/board.js
@@ -1,47 +1,64 @@
-import React, { useState } from 'react'
-import Cell from './cell'
-import 'bootstrap/dist/css/bootstrap.css'
+import React, { useState } from "react";
+import Cell from "./cell";
+import "bootstrap/dist/css/bootstrap.css";
 
 /** Create the board */
-const createBoard = (x, y) => {
-  let arr = []
+const initializeBoard = (x, y) => {
+  let arr = [];
   const initCell = {
     isHidden: false,
     isMarked: false,
     value: 0
-  }
+  };
   for (let i = 0; i < x; i++) {
-    let row = []
+    let row = [];
     for (let j = 0; j < y; j++) {
-      row.push({ ...initCell })
+      row.push({ ...initCell });
     }
-    arr.push([...row])
+    arr.push([...row]);
   }
-  return arr
-}
+  return arr;
+};
 
 const getRandomInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min
-}
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-/** Fill the board with mines */
-const putMines = (board, density, x, y) => {
-  let numberOfMines = Math.floor(x * y * density)
+/** Create list of mines */
+const createMines = (density, x, y) => {
+  let numberOfMines = Math.floor(x * y * density);
+  const mines = [];
   while (numberOfMines) {
-    let _x = getRandomInteger(0, x)
-    let _y = getRandomInteger(0, y)
-    if (board[_x][_y] !== -1) {
-      board[_x][_y] = { ...board[_x][_y], value: -1 }
-      numberOfMines--
+    let coord = { x: getRandomInteger(0, x), y: getRandomInteger(0, y) };
+    if (!mines.includes(coord)) {
+      mines.push(coord);
+      numberOfMines--;
     }
   }
-  return board
-}
+  return mines;
+};
+
+/** Fill the board with mines */
+const placeMines = (board, mines) => {
+  mines.forEach(coord => (board[coord.x][coord.y].value = -1));
+  return board;
+};
+
+/** Fill the board with values of bomb neighbors */
+const setBombNeighbors = (board, mines) => {
+  return board;
+};
+
+const createBoard = (cols, rows, density) => {
+  let board = initializeBoard(cols, rows);
+  placeMines(board, createMines(density, cols, rows));
+  setBombNeighbors(board);
+  return board;
+};
 
 function Board() {
-  const size = { x: 6, y: 6 }
-  let arrBoard = createBoard(size.x, size.y)
-  arrBoard = putMines(arrBoard, 0.5, size.x, size.y)
+  const size = { x: 6, y: 6 }; // Fixed size of board
+  let arrBoard = createBoard(size.x, size.y, 0.5); // Fixed density
 
   return (
     <div>
@@ -55,7 +72,7 @@ function Board() {
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Board
+export default Board;
